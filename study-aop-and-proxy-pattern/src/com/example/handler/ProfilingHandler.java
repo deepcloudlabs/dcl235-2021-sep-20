a199af1e6ef7bc8b8f9f14a13795fb8e01c64c6b
deepcloudlabs <deepcloudlabs@gmail.com>
2021-09-21 14:34:11 +0300

    initial commit

---
+++ study-aop-and-proxy-pattern/src/com/example/handler/ProfilingHandler.java
@@ -0,0 +1,25 @@
+package com.example.handler;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Date;
+
+public class ProfilingHandler implements InvocationHandler {
+	private Object target;
+	
+	public ProfilingHandler(Object target) {
+		this.target = target;
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		var methodName = method.getName();
+		long start = System.nanoTime();
+		var result = method.invoke(target, args);
+		long stop = System.nanoTime();
+		System.err.println(String.format("Method (%s) runs %d ns.", methodName,(stop-start)));		
+		return result;
+	}
+
+}
